4a70a73c73994792c25180f1be44a7b93997e943
Maue <chris.maue@intelligrated.com>
2017-09-19 10:58:50 -0500

    Added some logging statements for debug purposes

--- bob_wemo_service/tools/ipv4_help.py
+++ bob_wemo_service/tools/ipv4_help.py
@@ -34,10 +34,15 @@ def check_ipv4(address, logger=None):
     if isinstance(address, str) is not True:
         try:
             address = str(address)
+            logger.debug('Input value was successfully converted to string')
         except Exception:
             return False
+    else:
+        logger.debug('Input value was not a string: %s', str(address))
     # check if address is formatted correctly for an ipv4 address
     if re.fullmatch(ipv4_regex, address) is not None:
+        logger.debug('Provided address is a valid IP address: %s', address)
         return True
     else:
+        logger.debug('Provided address is NOT a valid IP address: %s', address)
         return False
--- bob_wemo_service/tools/ref_num.py
+++ bob_wemo_service/tools/ref_num.py
@@ -26,7 +26,9 @@ class RefNum(object):
     def __init__(self, logger=None):
         # Configure loggers
         self.logger = logger or logging.getLogger(__name__)
+
         # Init tags
+        self.logger.debug('Setting source register to initial value of 100')
         self._source = 100
 
     # source control **********************************************************
@@ -48,7 +50,10 @@ class RefNum(object):
 
     # new value control *******************************************************
     def new(self):
+        self.logger.debug('Incrementing source register')
         self._source += 1
         if self._source > 999:
+            self.logger.debug('Rolling over source register')
             self._source = 100
+        self.logger.debug('Returning source value to main: %d', self._source)
         return str(self._source)
